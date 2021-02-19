43aab1ec60f52b6a584d42f6b2132d2c353e317c
nataraj2 <nataraj2@illinois.edu>
2021-02-19 15:59:58 -0700

    hard coding EB inflow test case

--- SourceCpp/MOL.cpp
+++ SourceCpp/MOL.cpp
@@ -311,11 +311,11 @@ pc_compute_hyp_mol_flux(
         double ycen = 0.0 + (j+0.5)*0.0078125;
 	double vx = 30000.0*cos(3.14150/4.0);
 	double vy = 30000.0*sin(3.14150/4.0);
-	if(ycen > 0.2 and ycen < 0.25){
+	if(ycen > 20.0 and ycen < 25.0){
 		double vn = vx*(-ebnorm[0])+vy*(-ebnorm[1]);
 		qtempr[R_UN] = 2.0*vn - qtempl[R_UN];
 	}
-	else if(ycen > 0.4 and ycen < 0.45){
+	else if(ycen > 40.0 and ycen < 45.0){
 		double vn = vx*(-ebnorm[0])+vy*(-ebnorm[1]);
 		qtempr[R_UN] = 2.0*vn - qtempl[R_UN];
 	}
